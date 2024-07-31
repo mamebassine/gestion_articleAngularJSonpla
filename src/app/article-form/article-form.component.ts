6b0fa9edffb82e80e9d9db00ae629b42cfcba8b5
mamebassine <mamebassine06@gmail.com>
2024-07-31 08:45:09 +0000

    Ajout

--- src/app/article-form/article-form.component.ts
+++ src/app/article-form/article-form.component.ts
@@ -10,8 +10,8 @@
 // export class ArticleFormComponent {
 
 // }
+
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../data.service';
 import { Article } from '../article.interface';
@@ -22,44 +22,32 @@ import { Article } from '../article.interface';
   styleUrls: ['./article-form.component.css']
 })
 export class ArticleFormComponent implements OnInit {
-  articleForm: FormGroup;
-  isEdit = false;
-  submitted = false;
+  article: Article = { title: '', body: '' };
+  isEdit: boolean = false;
 
   constructor(
-    private fb: FormBuilder,
     private dataService: DataService,
     private route: ActivatedRoute,
     private router: Router
-  ) {
-    this.articleForm = this.fb.group({
-      title: ['', Validators.required],
-      body: ['', Validators.required]
-    });
-  }
+  ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.isEdit = true;
-      this.dataService.getArticle(Number(id)).subscribe(article => {
-        this.articleForm.patchValue(article);
+      this.dataService.getArticle(+id).subscribe(article => {
+        this.article = article;
       });
     }
   }
 
   onSubmit(): void {
-    this.submitted = true;
-    if (this.articleForm.invalid) {
-      return;
-    }
     if (this.isEdit) {
-      const id = Number(this.route.snapshot.paramMap.get('id'));
-      this.dataService.updateArticle({ ...this.articleForm.value, id }).subscribe(() => {
+      this.dataService.updateArticle(this.article).subscribe(() => {
         this.router.navigate(['/articles']);
       });
     } else {
-      this.dataService.createArticle(this.articleForm.value).subscribe(() => {
+      this.dataService.createArticle(this.article).subscribe(() => {
         this.router.navigate(['/articles']);
       });
     }
