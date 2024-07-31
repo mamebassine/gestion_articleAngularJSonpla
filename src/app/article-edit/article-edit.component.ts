3c09757555652875496628c5a273a5a5339c6b23
mamebassine <mamebassine06@gmail.com>
2024-07-31 16:03:11 +0000

    Es modificationss.

---
+++ src/app/article-edit/article-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-edit.component.html',
+  styleUrl: './article-edit.component.css'
+})
+export class ArticleEditComponent {
+
+}
