afbb3f93612bce538c0bb3da29af2682fb9d1971
mamebassine <mamebassine06@gmail.com>
2024-07-31 14:08:28 +0000

    Ajout detailArticle.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleFormComponent } from './article-form/article-form.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component'; // Assurez-vous d'importer ce composant
+
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/articles', pathMatch: 'full' },
   { path: 'articles', component: ArticleListComponent },
   { path: 'articles/new', component: ArticleFormComponent },
-  { path: 'articles/edit/:id', component: ArticleFormComponent }
+  { path: 'articles/edit/:id', component: ArticleFormComponent },
+  { path: 'articles/:id', component: ArticleDetailComponent },  // Nouvelle route
+
 ];
--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-article-detail',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './article-detail.component.html',
+//   styleUrl: './article-detail.component.css'
+// })
+// export class ArticleDetailComponent {
+
+// }
+
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
+import { Article } from '../article.interface';
 
 @Component({
   selector: 'app-article-detail',
-  standalone: true,
-  imports: [],
   templateUrl: './article-detail.component.html',
-  styleUrl: './article-detail.component.css'
+  styleUrls: ['./article-detail.component.css']
 })
-export class ArticleDetailComponent {
+export class ArticleDetailComponent implements OnInit {
+  article: Article | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.dataService.getArticle(id).subscribe(article => {
+      this.article = article;
+    });
+  }
+
+  backToList(): void {
+    window.history.back();
+  }
 }
