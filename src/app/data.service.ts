bfaef2341a42bc48fce2b5df55911c456e944ded
mamebassine <mamebassine06@gmail.com>
2024-08-01 18:10:08 +0000

    Modification

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { Article } from './article.interface';
 import { Comment } from './comment.interface';
 
