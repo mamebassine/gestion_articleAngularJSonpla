97ceefa944cb8ac8c7cc081f46178f957ac6c3a6
mamebassine <mamebassine06@gmail.com>
2024-08-01 11:03:37 +0000

    Modifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { Routes } from '@angular/router';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleFormComponent } from './article-form/article-form.component';
-import { ArticleDetailComponent } from './article-detail/article-detail.component'; // Assurez-vous d'importer ce composant
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/articles', pathMatch: 'full' },
   { path: 'articles', component: ArticleListComponent },
   { path: 'articles/new', component: ArticleFormComponent },
-  // { path: 'articles/edit/:id', component: ArticleFormComponent },
-
+  
   { path: 'articles/edit/:id', component: ArticleFormComponent }, // Modification
-
-  { path: 'articles/:id', component: ArticleDetailComponent } // Route pour voir les détails d'un article
+{ path: 'articles/:id', component: ArticleDetailComponent } // Route pour voir les détails d'un article
 ];
